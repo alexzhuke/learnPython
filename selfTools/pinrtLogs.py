1faaaea4df7e5e5c734bdfa69785d6f90b2e9b8e
alexzhuke <alexee_zhu@163.com>
2014-10-07 20:54:41 +0800

    add printlog script

---
+++ selfTools/pinrtLogs.py
@@ -0,0 +1,14 @@
+__author__ = 'modoojunko'
+
+#this scripts is used to print logs for all scripts
+
+
+def print_head(content):
+    print "+"*(len(content) + 2)
+    print "|" + content + "|"
+    print "+"*(len(content) + 2)
+
+
+
+
+print_head("good mornning! ChenSha,how are you")
