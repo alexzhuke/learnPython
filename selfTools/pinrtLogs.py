a08910e3302f1d80514a4f6d00186e7876779978
alexzhuke <alexee_zhu@163.com>
2014-10-07 21:23:32 +0800

    add sub_head and title print method

--- selfTools/pinrtLogs.py
+++ selfTools/pinrtLogs.py
@@ -1,14 +1,28 @@
 __author__ = 'modoojunko'
 
 #this scripts is used to print logs for all scripts
-content = "Hello World!"
+contents = "Hello World!"
+
+
+def print_factor(content, mark):
+    print mark*(len(content) + 2*len(mark))
+    print mark + content + mark
+    print mark*(len(content) + 2*len(mark))
+
 
 def print_head(content):
-    print "+"*(len(content) + 2)
-    print "|" + content + "|"
-    print "+"*(len(content) + 2)
+    print_factor(content, "#")
 
 
+def print_sub_head(content):
+    print_factor(content, "+")
 
 
-#print_head(content)
+def print_title(content):
+    print_factor(content, "-")
+
+
+if __name__ == "__main__":
+    print_head(contents)
+    print_sub_head(contents)
+    print_title(contents)
