678e8069b596dcdf7fc51ce7ad28475d95706d1d
alexzhuke <alexee_zhu@163.com>
2014-10-08 20:52:31 +0800

    add time& script name method

--- selfTools/pinrtLogs.py
+++ selfTools/pinrtLogs.py
@@ -1,5 +1,7 @@
 __author__ = 'modoojunko'
 
+import time
+
 #this scripts is used to print logs for all scripts
 contents = "Hello World!"
 
@@ -8,6 +10,17 @@ def print_factor(content, mark):
     print mark*(len(content) + 2*len(mark))
     print mark + content + mark
     print mark*(len(content) + 2*len(mark))
+
+
+def time_factor():
+    local_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+    return local_time
+
+
+def log_factory(content):
+    current_time = time_factor()
+    file_name = __file__.split("/")[-1]
+    print "[" + current_time + " " + file_name + "]" + content
 
 
 def print_head(content):
@@ -26,8 +39,10 @@ def print_sub_title(content):
     print_factor(content, ":")
 
 
+
 if __name__ == "__main__":
     print_head("head")
     print_sub_head("sub_head")
     print_title("title")
     print_sub_title("sub_title")
+    log_factory("it is time to show the detail logs")
