efcc6ac67d73a8842e42abf1710e36dd2a27bf6f
alexzhuke <alexee_zhu@163.com>
2014-10-10 13:14:46 +0800

    add time& script name method

--- selfTools/findPathForFile.py
+++ selfTools/findPathForFile.py
@@ -5,5 +5,5 @@ import learnOs.learnOSlistDir.learnOSlistDir
 import pinrtLogs
 path = "/Users/modoojunko/zhuke"
 pinrtLogs.print_head("checking file list")
-file_list = learnOs.learnOSWalk2.go_through_path(path)
-file_list2 = learnOs.learnOSlistDir.learnOSlistDir.listdir(path)
+#file_list = learnOs.learnOSWalk2.go_through_path(path)
+#file_list2 = learnOs.learnOSlistDir.learnOSlistDir.listdir(path)
